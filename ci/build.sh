deb5f3ec903c6857953f7cefe4662bf4695be296
Ivan Krutov <vania-pooh@vania-pooh.com>
2020-07-20 20:17:47 +0300

    Fixed workspace in .drone.yml

--- ci/build.sh
+++ ci/build.sh
@@ -6,6 +6,7 @@ output_dir=${1:-"/tmp"}
 version=${2:-"latest"}
 mkdir -p ${output_dir}
 tar c -vz -C chart ${output_dir}/moon-${version}.tgz moon
-cd ${output_dir}
+pushd ${output_dir}
 wget ${CHARTS_REPO}/index.yaml
 helm repo index --url ${CHARTS_REPO} --merge index.yaml
+popd
