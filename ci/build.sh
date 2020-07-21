07fbd3f8188c3f72bdf2beda782474d1e5458ed0
Ivan Krutov <vania-pooh@vania-pooh.com>
2020-07-21 07:26:53 +0300

    Correctly archiving chart

--- ci/build.sh
+++ ci/build.sh
@@ -5,8 +5,7 @@ CHARTS_REPO="https://charts.aerokube.com/"
 output_dir=${1:-"/tmp"}
 version=${2:-"latest"}
 mkdir -p ${output_dir}
-tar c -vz -C chart ${output_dir}/moon-${version}.tgz moon
-pushd ${output_dir}
+tar cvz -C chart -f ${output_dir}/moon-${version}.tgz moon
+cd ${output_dir}
 wget ${CHARTS_REPO}/index.yaml
 helm repo index --url ${CHARTS_REPO} --merge index.yaml
-popd
